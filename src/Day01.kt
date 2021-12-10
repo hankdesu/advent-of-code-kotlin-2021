1dfe0af88a7a3b35e5857631086bae11700c086e
Hank <hankchou@climax.com.tw>
2021-12-10 08:45:14 +0000

    feat(day 01): add day 1 part 1 function

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,16 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var measurements = 0
+        var lastNumber = 0
+        for((index, number) in input.withIndex()) {
+            if (index != 0) {
+                if (number.toInt() > lastNumber) {
+                    measurements += 1
+                }
+            }
+            lastNumber = number.toInt()
+        }
+        return measurements
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +19,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
